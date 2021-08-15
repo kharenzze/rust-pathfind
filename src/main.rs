afa17aa9ce03bdb9448ec403a2cce1c777114a3d
Paolo <str237@gmail.com>
2021-08-15 13:26:41 +0200

    Init logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use simple_logger::{SimpleLogger};
+use log::{LevelFilter, info};
+
 fn main() {
-    println!("Hello, world!");
+  SimpleLogger::new()
+    .with_colors(true)
+    .with_level(LevelFilter::Error)
+    .with_module_level("pathfind", LevelFilter::Debug)
+    .init()
+    .unwrap();
+
+  info!("Hello, world!");
 }
