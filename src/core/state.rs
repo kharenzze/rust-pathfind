f45c97315ed49edf6fa425f022b867e97f279fda
Paolo <str237@gmail.com>
2021-08-15 16:38:36 +0200

    Impl EventHandler

--- src/core/state.rs
+++ src/core/state.rs
@@ -1,1 +1,13 @@
+use ggez::event::EventHandler;
+use ggez::error::{GameResult, GameError};
 pub struct MainState {}
+
+impl EventHandler<GameError> for MainState {
+  fn update(&mut self, _ctx: &mut ggez::Context) -> GameResult {
+    Ok(())
+  }
+
+  fn draw(&mut self, _ctx: &mut ggez::Context) -> GameResult {
+    Ok(())
+  }
+}
