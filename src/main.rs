e9d094ab49bc0e0db0f8a949b409813c3697d217
Paolo <str237@gmail.com>
2021-08-15 19:57:01 +0200

    Constants

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use pathfind::game::MainState;
 
 const RES_1080: Vec2 = const_vec2!([1920.0, 1080.0]);
 
+const TITLE: &str = "Title";
+const AUTHOR: &str = "Author";
+const GAME_ID: &str = "GAME_ID";
+
 fn main() {
   SimpleLogger::new()
     .with_colors(true)
@@ -14,18 +18,18 @@ fn main() {
     .init()
     .unwrap();
 
-  info!("Hello, world!");
-  let window_setup = ggez::conf::WindowSetup::default().title("Steering");
+  info!("Start!");
+  let window_setup = ggez::conf::WindowSetup::default().title(TITLE);
   let res = RES_1080;
   let window_mode = ggez::conf::WindowMode::default()
     .min_dimensions(res.x, res.y)
     .dimensions(res.x, res.y);
   // Make a Context.
-  let (ctx, event_loop) = ContextBuilder::new("Steering", "Kharenzze")
+  let (ctx, event_loop) = ContextBuilder::new(GAME_ID, AUTHOR)
     .window_setup(window_setup)
     .window_mode(window_mode)
     .build()
-    .expect("aieee, could not create ggez context!");
+    .expect("Could not create ggez context!");
 
   let state = MainState{};
 
